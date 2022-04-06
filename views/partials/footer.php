5009593c24916267f8eb1bfac48155b2ba304a8e
Daniyal Z <zdaniyalr@gmail.com>
2022-04-06 10:17:05 +0600

    макет авторизации

---
+++ views/partials/footer.php
@@ -0,0 +1,40 @@
+<?php
+// close conn
+?>
+    <div class="container">
+        <footer class="pt-4 my-md-5 pt-md-5 border-top">
+            <div class="row">
+                <div class="col-12 col-md">
+                    <!-- <img class="mb-2" src="../../assets/brand/bootstrap-solid.svg" alt="" width="24" height="24"> -->
+                    <small class="d-block mb-3 text-muted">&copy; 2022</small>
+                </div>
+                <div class="col-6 col-md">
+                    <h5>Разработали проект:</h5>
+                    <ul class="list-unstyled text-small">
+                        <li><a class="text-muted" href="#">Жантурин Д.Р.</a></li>
+                        <li><a class="text-muted" href="#">Долгушин Н.Л.</a></li>
+                        <li><a class="text-muted" href="#">Серикова Д.Л.</a></li>
+                    </ul>
+                </div>
+                <!-- <div class="col-6 col-md">
+                    <h5>Resources</h5>
+                    <ul class="list-unstyled text-small">
+                        <li><a class="text-muted" href="#">Resource</a></li>
+                        <li><a class="text-muted" href="#">Resource name</a></li>
+                        <li><a class="text-muted" href="#">Another resource</a></li>
+                        <li><a class="text-muted" href="#">Final resource</a></li>
+                    </ul>
+                </div> -->
+                <div class="col-6 col-md">
+                    <h5>Справка</h5>  
+                    <ul class="list-unstyled text-small">
+                        <li><a class="text-muted" href="#">Team</a></li>
+                        <li><a class="text-muted" href="#">Locations</a></li>
+                        <li><a class="text-muted" href="#">Privacy</a></li>
+                        <li><a class="text-muted" href="#">Terms</a></li>
+                    </ul>
+                </div>
+            </div>
+        </footer>
+    </div>
+</body>
